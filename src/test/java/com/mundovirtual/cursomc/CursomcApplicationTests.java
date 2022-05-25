1d774f0252c9ad871be5cf9fcf53679cdfb96421
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2022-05-25 17:49:21 -0300

    Projeto criado

---
+++ src/test/java/com/mundovirtual/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mundovirtual.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
